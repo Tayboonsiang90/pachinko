c6b40b2e082fa08d81d7de4150ba0097e23f9fc7
unknown <tayboonsiang90@gmail.com>
2022-03-12 16:39:49 +0800

    Added more trader helper functions

--- index.js
+++ index.js
@@ -7,47 +7,54 @@ async function createTrader(name, country, dateOfBirth, email, password) {
     await getDB().collection("trader").insertOne({ name, country, dateOfBirth, email, password });
 }
 //This function takes in a submitted email and password and retrieves the trader details
+//It will return a falsely value of the trader isn't found, otherwise it will be the full trader details
 async function loginTrader(email, password) {
-    await getDB().collection("trader").find
-
+    let trader = await getDB().collection("trader").findOne({ email, password });
+    return trader;
 }
 //This function takes in a submitted email and if email already being used
+//Returns true if its being used, false if its not being used
 async function checkTraderEmail(email) {
-    await getDB().collection("trader").find({
-        
-    })
-
+    let trader = await getDB().collection("trader").findOne({ email });
+    return trader ? true : false;
 }
 //This function takes in validated email and changes the email of the trader with Id_
 async function changeTraderEmail(traderId_, email) {
-    await getDB().collection("trader").updateOne({
-        '_id': ObjectId(traderId_)
-    }, {
-        '$set': {email}
-    })
+    await getDB()
+        .collection("trader")
+        .updateOne(
+            {
+                _id: ObjectId(traderId_),
+            },
+            {
+                $set: { email },
+            }
+        );
 }
 //This function takes in validated password and changes the password of the trader with Id_
 async function changeTraderPassword(traderId_, password) {
-    await getDB().collection("trader").updateOne({
-        '_id': ObjectId(traderId_)
-    }, {
-        '$set': {password}
-    })
+    await getDB()
+        .collection("trader")
+        .updateOne(
+            {
+                _id: ObjectId(traderId_),
+            },
+            {
+                $set: { password },
+            }
+        );
 }
-
-
-
-
 
 //Create Account Stack Ends
 
 async function main() {
     const MONGO_URI = process.env.MONGO_URI;
     await connect(MONGO_URI, "pachinko");
-    console.log(getDB);
 
-    allCoins = await getDB().collection("coin").find().toArray();
-    console.log(allCoins);
+    // allCoins = await getDB().collection("coin").find().toArray();
+    // console.log(allCoins);
+
+    console.log(await checkTraderEmail("tayboonsiang90@gmail.com"));
 }
 
 main();
